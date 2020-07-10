6041f009aef1bb6dc7c599f2267305776a38a43a
KittiST <kittisq@gmail.com>
2020-07-10 18:39:26 +0700

    improve

--- src/App.js
+++ src/App.js
@@ -4,29 +4,51 @@ import CustomCam from './CustomCam';
 
 function App() {
   const [isMrror, setIsMirror] = useState(false)
+  const [isActive, setIsActive] = useState(null)
   const [getImage, setImage] = useState(null)
   const [camData, setCamData] = useState({
     currentIndex: 0,
     list: [],
   });
-  const getC = useCallback((a) => setCamData(a), [setCamData]) 
+  const getCamData = useCallback((a) => {setCamData(a); console.log(a)}, [setCamData]) 
   const updateM = () => isMrror ? setIsMirror(false) : setIsMirror(true)
-
   const CustomCamRef = useRef();
 
+  const toggleActive = () => {
+    CustomCamRef.current.toggleCamActive()
+    setIsActive(!isActive)
+  }
+
+  const switchCam = (val) => {
+    CustomCamRef.current.switchCam(val)
+    setCamData({currentIndex: val, list: camData.list})
+  }
+
   const handleCapture = () => {
-    let t = CustomCamRef.current.getSnap('snap-div')
-    setImage(t)
+    let result = CustomCamRef.current.getSnap()
+    console.log(result)
+    setImage(result.base64)
+    const canvas = document.getElementById("canvas-div");
+    const ctx = canvas.getContext("2d");
+    const image = new Image();
+    image.onload = () => ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    image.src = result.base64
+  }
+  const handleCapture2 = () => {
+    let result = CustomCamRef.current.getCanvas("canvas-div")
+    console.log(result)
   }
 
   return (
     <div id='App'>
-      <button onClick={() => handleCapture()}>Capture</button>
+      <button onClick={() => toggleActive()}>{ isActive ? 'Play' : 'Pause' }</button>
+      <button onClick={() => handleCapture2()} disabled={isActive}>Capture</button>
       <button onClick={() => updateM()}> Mirror : {isMrror.toString() }</button>
       <div>
-          <select value={camData.currentIndex} onChange={(e) => setCamData({currentIndex: Number(e.target.value), list: camData.list})}>
+          {/* <select value={camData.currentIndex} onChange={(e) => setCamData({currentIndex: Number(e.target.value), list: camData.list})}> */}
+          <select value={camData.currentIndex} onChange={(e) => switchCam(Number(e.target.value))}>
             {
-              camData.list.map((ea_device,i) => (
+              camData.list.map((ea_device, i) => (
                 <option value={i} key={i}>{ea_device.label}</option>
               ))
             }
@@ -34,14 +56,14 @@ function App() {
         </div>
       <CustomCam
         ref={CustomCamRef}
-        getCamData={getC}
-        currentIndex={camData.currentIndex}
-        mirror={isMrror}
+        getCamData={getCamData}
+        // currentIndex={camData.currentIndex}
+        // mirror={isMrror}
         width={1024}
         height={576}
       />
-      <canvas className='snap-div' id='snap-div'/>
       <img className='img-div' src={getImage} alt=""/>
+      <canvas className='canvas-div' id='canvas-div'/>
       
     </div>
     
--- src/CustomCam.js
+++ src/CustomCam.js
@@ -1,17 +1,51 @@
 import React, { useState, useEffect, forwardRef, useImperativeHandle } from 'react';
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles(() => ({
+  root: {
+    position: 'relative',
+    height: '100%',
+    width: '100%'
+  },
+  container: {
+    position: 'absolute',
+    top: '0',
+    bottom: '0',
+    width: '100%',
+    height: '100%', 
+    overflow: 'hidden',
+  },
+  video: {
+    minWidth: '100%',
+    minHeight: '100%',
+    width: 'auto',
+    height: 'auto',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%,-50%)',
+    zIndex: '0'
+  },
+  mirror: {
+    transform: 'translate(-50%,-50%) scale(-1,1) !important'
+  },
+  canvas: {
+    display: 'none'
+  }
+}))
 
 const CustomCamera = forwardRef(
   ({ getCamData, currentIndex, mirror, height, width }, ref) => {
-    const [isInit, setInit] = useState(false);
-    const [deviceId, setDeviceId] = useState(0);
+    const [isInit, setInit] = useState(null);
+    const [deviceId, setDeviceId] = useState(null);
     const [deviceList, setDeviceList] = useState([]);
     const [isMirror, setIsMirror] = useState(false);
 
     useImperativeHandle(ref, () => ({
-      getSnap(canvasId) {
+      getSnap() {
         let videoEl = document.getElementById('inputVideo')
         if (videoEl.paused || videoEl.ended) return null
-        let canvas = document.getElementById(canvasId)
+        let canvas = document.getElementById('custom-cam-video-canvas')
         canvas.width = videoEl.videoWidth;
         canvas.height = videoEl.videoHeight;
         let ctx = canvas.getContext('2d')
@@ -20,11 +54,36 @@ const CustomCamera = forwardRef(
           ctx.scale(-1, 1);
         }
         ctx.drawImage(videoEl, 0, 0, videoEl.videoWidth, videoEl.videoHeight);
-        var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"); 
-        return image
+        const base64 = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"); 
+        return { base64, size: [videoEl.videoWidth, videoEl.videoHeight] }
+      },
+      getCanvas(canvasId) {
+        let videoEl = document.getElementById('inputVideo')
+        if (videoEl.paused || videoEl.ended) return null
+        let canvas = document.getElementById(canvasId)
+        // let canvas = document.getElementById('custom-cam-video-canvas')
+        canvas.width = videoEl.videoWidth;
+        canvas.height = videoEl.videoHeight;
+        let ctx = canvas.getContext('2d')
+        if (isMirror) {    
+          ctx.translate(videoEl.videoWidth, 0);
+          ctx.scale(-1, 1);
+        }
+        ctx.drawImage(videoEl, 0, 0, videoEl.videoWidth, videoEl.videoHeight);
+        return canvas
+      },
+      toggleCamActive() {
+        let videoEl = document.getElementById('inputVideo')
+        videoEl.paused ? videoEl.play() : videoEl.pause()
+      },
+      toggleMirror() {
+        setIsMirror(!isMirror)
+      },
+      switchCam(cam) {
+        console.log(cam)
+        setDeviceId(cam)
       }
     }));
-
 
     useEffect(() => {
       const updateVideo = async (newIndex) => {
@@ -43,12 +102,16 @@ const CustomCamera = forwardRef(
           video.play();
         });
       }
-      if (currentIndex !== deviceId && isInit) {
-        updateVideo(deviceList[currentIndex].deviceId)
-        setDeviceId(currentIndex)
+      // if (currentIndex && currentIndex !== deviceId && !isInit) {
+      //   updateVideo(deviceList[currentIndex].deviceId)
+      //   setDeviceId(currentIndex)
+      // } 
+      console.log('kk' + deviceId + isInit)
+      if (deviceId !== null && !isInit) {
+        updateVideo(deviceList[deviceId].deviceId)
+        // setDeviceId(deviceId)
       } 
-        
-    }, [isInit, currentIndex, deviceId, deviceList])
+    }, [isInit, deviceId, deviceList])
     useEffect(() => {
       const launchVideo = async () => {
         const video = document.getElementById('inputVideo');
@@ -56,39 +119,41 @@ const CustomCamera = forwardRef(
         if (height && width) video.style.cssText = videoV.style.cssText = `height: ${height}px !important; width: ${width}px !important`
         navigator.getMedia = (navigator.getUserMedia);
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-          const listDevicesX =  await navigator.mediaDevices.enumerateDevices();
-          const listDevices = await listDevicesX.filter(ea => ea.kind === 'videoinput')
-          setDeviceList(listDevices)
-          setDeviceId(listDevices.length-1)
-          getCamData({
-            currentIndex: listDevices.length-1,
-            list: listDevices,
-          })
           const constraints = {
             audio: false,
             video: {
-              deviceId: { exact: listDevices[listDevices.length-1].deviceId },
               width: { min: 1024, ideal: videoV?.clientWidth, max: 1920 },
               height: { min: 576, ideal: videoV?.clientHeight, max: 1080 },
             }
           }
-          navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-            video.srcObject = stream;
-            video.play();
+          navigator.mediaDevices.getUserMedia(constraints).then(async (stream) => {
+          const listDevicesX =  await navigator.mediaDevices.enumerateDevices();
+          const listDevices = await listDevicesX.filter(ea => ea.kind === 'videoinput')
+          setDeviceList(listDevices)
+          // setDeviceId(listDevices.length-1)
+          getCamData({
+            currentIndex: listDevices.length-1,
+            list: listDevices,
+            size: [videoV?.clientWidth, videoV?.clientHeight]
+          })
+          video.srcObject = stream;
+          video.play();
           });
         }
       }
       const init = async () => {
         await launchVideo()
-        setInit(true)
+        setInit(false)
       }
-      init()
+      init() 
     }, [setInit, getCamData, height, width])
-    useEffect(() => setIsMirror(mirror), [mirror, setIsMirror])
+    useEffect(() => {if (mirror !== null) setIsMirror(mirror)}, [mirror, setIsMirror])
+    const classes = useStyles()
     return (
-      <div className='main-cam-div' style={{ position: 'relative', height: '100%', width: '100%' }}>      
-        <div id='custom-cam-video-container'>
-          <video id="inputVideo" className={isMirror ? 'custom-cam-mirrored' : ''} autoPlay muted playsInline></video>
+      <div className={classes.root}>      
+        <div id='custom-cam-video-container' className={classes.container}>
+          <video id="inputVideo" className={`${classes.video} ${isMirror ? classes.mirror : ''}`} autoPlay muted playsInline></video>
+          <canvas id='custom-cam-video-canvas' className={classes.canvas} />
         </div>
       </div>
     );
